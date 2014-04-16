7af45e8157b48a09e1f284f08ba1597f5d83887c
Laura Chen <linkinmonkey@gmail.com>
2014-04-16 13:03:26 -0400

    update

--- server.js
+++ server.js
@@ -3,41 +3,50 @@ var express = require('express');
 
 //handlebars
 var expressHbs = require('express3-handlebars');
-var handlebars = expressHbs.create();
+var handlebars = expressHbs.create({
+    defaultLayout: 'main'
+});
 
+var tutorialData = require('./tutorialDataFile');
 var app = express();
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+//dynamic route
+app.get('/tutorial/:tutorialName', function(req, res){
+  var tutorialName = req.params.tutorialName;
+  var data = tutorialData[tutorialName];
 
-
-
-
-var person = require('./defaultValues.js');
-
-app.get('/', function(req, res){
-  // res.send('Hello World');
-  // res.sendfile('./public/index.html');
-  // res.send('Your name is: ' +
-  //           person.myName);
-
-  //handlebars render the temple of index
-  //variable into templete
-  var data = { planet: 'Earth'};
-  var planetNames = [
-    'Mars',
-    'Venus',
-    'Mercury'
-  ];
-  data.planetNames = planetNames;
-  res.render('index', data);
-
+  res.render('tutorial', data);
 });
 
-app.get('/about', function(req, res){
-  res.send('The About Page');
-});
+
+// var person = require('./defaultValues.js');
+
+// app.get('/', function(req, res){
+//   // res.send('Hello World');
+//   // res.sendfile('./public/index.html');
+//   // res.send('Your name is: ' +
+//   //           person.myName);
+
+//   //handlebars render the temple of index
+//   //variable into templete
+//   var data = { planet: 'Earth'};
+//   var planetNames = [
+//     'Mars',
+//     'Venus',
+//     'Mercury'
+//   ];
+//   data.planetNames = planetNames;
+//   res.render('index', data);
+
+// });
+
+
+// app.get('/about', function(req, res){
+//   res.send('The About Page');
+// });
 
 //the middle where
 //add it as a filter
---
+++ tutorialDataFile.js
@@ -0,0 +1,12 @@
+var data = {
+	'mushroom-number-one': {
+		name: 'Shiitake',
+		taste: 'good'
+	},
+	'portabella': {
+		name: 'Portabella',
+		taste: 'tasty'
+	}
+}
+
+module.exports = data;
